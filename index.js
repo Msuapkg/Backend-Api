56bfd984d2a2dedbda119d34106a6b308f18084b
msuapkg <msuapkg@gmail.com>
2022-04-25 21:42:12 -0500

    Conexion a PokeApi

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/api/v1', function (req, res) {
   res.send('Hola Mundo!')
 });
 
 app.listen(3000, () => {
-    console.log(`Hola desde el puerto: ${3000}`);
+  console.log(`Hola desde el puerto: ${3000}`);
 });
+
+let pokeApi = 'https://pokeapi.co/api/v2/pokemon/dragonite';
+
+async function user() {
+  const response = await axios.get(pokeApi);
+  try {
+    let data = response.data;
+    let moves = data.moves.map(item => item.move.name);
+    console.log(moves);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+user();
--- rutas.js
+++ rutas.js
@@ -1,4 +1,2 @@
-const express =  require("express")
-const app = express();
-
-app.get ('/', ()=> 'por fin guau aprendio');
+/* const express =  require("express")
+const app = express(); */
